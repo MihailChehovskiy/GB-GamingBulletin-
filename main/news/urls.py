679bfa4481911a795f1d696c3300dbf01aa0c057
Cheovskiy <mihail.tchehovsky@yandex.ru>
2024-03-15 12:37:40 +0300

    several markup files have been added and the transition is enabled

--- main/news/urls.py
+++ main/news/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about-us', views.about, name='about'),
     path('create', views.create, name='create'),
+    path('all_news', views.all_news, name='anews'),
+    path('game_news', views.game_news, name='game'),
+    path('news', views.news, name='news_us'),
 ]
--- main/news/views.py
+++ main/news/views.py
@@ -30,3 +30,24 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', context)
+
+
+def all_news(request):
+    public = publications.objects.order_by('-id')[:10]
+    return render(request, 'news/all_news.html',
+                  {'title': 'Страница со всеми новостями',
+                   'public': public})
+
+
+def game_news(request):
+    public = publications.objects.order_by('-id')[:10]
+    return render(request, 'news/game_news.html',
+                  {'title': 'Страница со всеми новостями',
+                   'public': public})
+
+
+def news(request):
+    public = publications.objects.order_by('-id')[:10]
+    return render(request, 'news/news.html',
+                  {'title': 'Страница со всеми новостями',
+                   'public': public})
